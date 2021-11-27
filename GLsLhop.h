5896b7530beb064501bb190e562d9209cd955fb9
Skade-code <zocker1206@gmail.com>
2021-11-28 00:49:01 +0100

    init

---
+++ GLsLhop.h
@@ -0,0 +1,8 @@
+﻿// GLsLhop.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
